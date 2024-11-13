16a431ccc2a73cad05cdda98a9e362f49b7e4ae5
HarshilTomar20 <harshiltomar20@gmail.com>
2024-11-13 20:40:47 +0530

    chore: bento 1 ai aligned

--- src/components/magicui/bento-grid.tsx
+++ src/components/magicui/bento-grid.tsx
@@ -45,17 +45,17 @@ const BentoCard = ({
     className={cn(
       "group relative col-span-3 flex flex-col justify-between overflow-hidden rounded-xl",
       // dark styles
-      "transform-gpu bg-black [border:1px_solid_rgba(255,255,255,.1)] [box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]",
+      "transform-gpu bg-black border border-blue-400/35 [box-shadow:0_-20px_80px_-20px_#3b82f61f_inset]",
       className,
     )}
   >
     <div>{background}</div>
     <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
       <Icon className="h-12 w-12 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75" />
-      <h3 className="text-xl font-semibold text-blue-500">
+      <h3 className="text-xl font-semibold text-blue-400">
         {name}
       </h3>
-      <p className="max-w-lg text-violet-200">{description}</p>
+      <p className="max-w-lg font-thin text-gray-400">{description}</p>
     </div>
 
     <div
